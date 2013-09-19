cbf7a370824b6f57eefa1136cd16cd52bdd3f783
Aniket <anidroid2@gmail.com>
2013-09-19 20:14:50 +0530

    v0.2

--- SensorFlawFilter.c
+++ SensorFlawFilter.c
@@ -1,19 +1,28 @@
-
-#include "stdio.h"
+#include <stdio.h>
 
 #define set(a,b) a|=(1<<b)
 #define clr(a,b) a&=(~(1<<b))
 #define chk(a,b) a&(1<<b)
 
-int main()
+#define bool int 
+#define true 1
+#define false 0
+int main(int argc, char **argv)
 {
-	unsigned char input=0xDD,output=0;
+	if(argc!=2)  //Runprogram only when one argument is passed.
+	{
+		printf("Incorrect format\n Please enter only one argument i.e. Input\n");
+		return 0;	
+	}
+	unsigned char input=atoi(argv[1]),output;
+	//Define input and output variables.
+	//atoi() converts string to int. 
+	printf("Input is %d \n",input);
+	
 	int i,flag=0,temp=0,max=0,total=0,max_temp;
 	int big=0,big_temp=0;
-	//bool isFault=0;
-	int isFault=0;
-	printf("Input was %d",input);	
-	getchar();
+	bool isFault=0;
+	//Initilize 
 	
 	//Analyzing start
 	for(i=0;i<8;i++)
@@ -42,20 +51,25 @@ int main()
 	{
 		isFault=1;
 	}
-
+	
 	//Analyzing End
-	printf("max is %d \n big is %d \n total is %d",max,big,total);
-	getchar();
-
+	printf("RESULT:\nmax   = %d \nbig   = %d \ntotal =%d\n",max,big,total);
+	if(isFault==true)
+	{
+		printf("Anomality Detected\n");
+	}
+	else
+	{
+		printf("Anomaly Not Detected\n");
+		return 0;
+	}	
 	//Filtering Start
 	for(i=0;i<big;i++)
 	{
 		set(output,max-i);
 	}
 	//Filtering End
-	printf("FILTERED OUTPUT  \t  %d \n",output);
+	printf("FILTERED OUTPUT  \t  %d \n\n",output);
 	getchar();
 	return 0;
 }
-
-
